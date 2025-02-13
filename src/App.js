d3bb80e4a4e345dc891e7e5ef3bd0bd20de490b6
ayushnilabh1986 <ayush.nilabh3@gmail.com>
2025-02-13 20:22:05 +0530

    https://github.com/ayushnilabh1986/portfoliomanager/issues/7 https://github.com/ayushnilabh1986/portfoliomanager/issues/8 https://github.com/ayushnilabh1986/portfoliomanager/issues/2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import googleLogo from "./../src/assets/google.webp";
 function App() {
   return (
     <div className="App">
+      <header>
       <h1 className="terolo"> Porfolio Manager </h1>
+      </header>
       <div className="box">
         <div className="box1">
           <h2 className="signIn">Sign in</h2>
@@ -15,7 +17,9 @@ function App() {
           </div>
 
           <form action="" className="form">
+            <hr className="line1" />
     <h1 className="form__title">Or sign in with Email</h1>
+    <hr className="line2" />
 
     <div className="form__group">
       <input type="text" id="email" className="form__input" placeholder=" " autocomplete="off" />
@@ -54,6 +58,9 @@ function App() {
           </div>
         </div>
       </div>
+      <footer className="footer">
+      <p>The site is protected by reCAPTCHA and the Google <a href="Privacy.jsx">Privacy Policy</a>  and <a href="Terms.jsx">Terms of Conditions</a> apply.</p>
+      </footer>
     </div>
   );
 }
